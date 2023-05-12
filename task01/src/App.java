519a67e22d58d885963fb8ac258d5abaf6cf398b
xltan98 <xinnli98@gmail.com>
2023-05-12 09:56:50 +0800

    first commit

---
+++ task01/src/App.java
@@ -0,0 +1,84 @@
+import java.io.Console;
+import java.util.LinkedList;
+import java.util.List;
+
+public class App {
+    private static final int LinkedList = 0;
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Welcome.");
+
+        Console cons = System.console();
+        String message="";
+        double answer;
+        String ans="0";
+
+        while (!"exit".equals(message.toLowerCase())){
+
+            message =cons.readLine(">");
+
+            if (message.contains("last")){
+                message = message.replaceAll("last",ans);
+            }
+
+            //List<String> messageList = new LinkedList<>();
+           String[] messageArray = message.split(" ");
+
+            answer = Double.parseDouble(ans);
+
+           String firstNumber=messageArray[0];
+           double firstNo = Double.parseDouble(firstNumber);
+           
+
+           String ariOperations = messageArray[1];
+           
+           String secondNumber = messageArray[2];
+           double secondNo = Double.parseDouble(secondNumber);
+
+
+              switch (ariOperations){
+                case "+":
+                answer =firstNo + secondNo;
+                ans=Double.toString(answer);
+                System.out.printf("This is your answer: %.2f\n",answer);
+                break;
+    
+                case "-":
+                answer =firstNo - secondNo;
+                ans=Double.toString(answer);
+                System.out.printf("This is your answer: %.2f\n",answer);
+                break;
+    
+                case"*":
+                answer =firstNo * secondNo;
+                ans=Double.toString(answer);
+                System.out.printf("This is your answer: %.2f\n",answer);
+                break;
+    
+                case "/":
+                answer =firstNo / (secondNo);
+                ans=Double.toString(answer);
+                System.out.printf("This is your answer: %.2f\n",answer);
+                break;
+                
+                default:
+                System.out.println("Your input is invalid");
+                break;
+            }
+
+            
+
+
+        }
+
+        System.out.println("Bye bye");
+
+        
+        //1.Accepting user input
+        //2.saving the last result
+        //3. creating switch case
+        //4. create while loop for exiting
+        
+    }
+}
---
+++ task02/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String dirpath = args[0];
+    }
+}
