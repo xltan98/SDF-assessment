eef699d7d035aebc9e3e614979e986abf59eb566
xltan98 <xinnli98@gmail.com>
2023-05-12 16:20:30 +0800

    final edit for task1

--- task01/src/App.java
+++ task01/src/App.java
@@ -1,6 +1,4 @@
 import java.io.Console;
-import java.util.LinkedList;
-import java.util.List;
 
 public class App {
     private static final int LinkedList = 0;
@@ -8,80 +6,77 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         System.out.println("Welcome.");
-
         Console cons = System.console();
-        String message="";
+        String message = "";
         double answer;
-        String ans="0";
-        //String oldMessage;
+        String ans = "0";
+        double firstNo = 0;
+        double secondNo = 0;
 
-        while (!"exit".equals(message.toLowerCase())){
+        while (!"exit".equals(message.toLowerCase())) {
 
-            message =cons.readLine(">");
-           // message = oldMessage.replaceAll("$","");
-            
+            message = cons.readLine(">");
 
-            if (message.contains("last")){
-                message = message.replaceAll("last",ans);
-            }
-
-            //List<String> messageList = new LinkedList<>();
-           String[] messageArray = message.split(" ");
+            String[] messageArray = message.split(" ");
 
             answer = Double.parseDouble(ans);
 
-           String firstNumber=messageArray[0];
-           double firstNo = Double.parseDouble(firstNumber);
-           
+            if (messageArray[0].equalsIgnoreCase("$last")) {
+                firstNo = answer;
 
-           String ariOperations = messageArray[1];
-           
-           String secondNumber = messageArray[2];
-           double secondNo = Double.parseDouble(secondNumber);
-
-
-              switch (ariOperations){
-                case "+":
-                answer =firstNo + secondNo;
-                ans=Double.toString(answer);
-                System.out.printf("This is your answer: %.2f\n",answer);
-                break;
-    
-                case "-":
-                answer =firstNo - secondNo;
-                ans=Double.toString(answer);
-                System.out.printf("This is your answer: %.2f\n",answer);
-                break;
-    
-                case"*":
-                answer =firstNo * secondNo;
-                ans=Double.toString(answer);
-                System.out.printf("This is your answer: %.2f\n",answer);
-                break;
-    
-                case "/":
-                answer =firstNo / (secondNo);
-                ans=Double.toString(answer);
-                System.out.printf("This is your answer: %.2f\n",answer);
-                break;
-                
-                default:
-                System.out.println("Your input is invalid");
-                break;
+            } else {
+                String firstNumber = messageArray[0];
+                firstNo = Double.parseDouble(firstNumber);
             }
 
-            
+            if (messageArray[2].equalsIgnoreCase("$last")) {
+                secondNo = answer;
 
+            } else {
+                String secondNumber = messageArray[2];
+                secondNo = Double.parseDouble(secondNumber);
+            }
+
+            String ariOperations = messageArray[1];
+
+            switch (ariOperations) {
+                case "+":
+                    answer = firstNo + secondNo;
+                    ans = Double.toString(answer);
+                    System.out.printf("This is your answer: %.2f\n", answer);
+                    break;
+
+                case "-":
+                    answer = firstNo - secondNo;
+                    ans = Double.toString(answer);
+                    System.out.printf("This is your answer: %.2f\n", answer);
+                    break;
+
+                case "*":
+                    answer = firstNo * secondNo;
+                    ans = Double.toString(answer);
+                    System.out.printf("This is your answer: %.2f\n", answer);
+                    break;
+
+                case "/":
+                    answer = firstNo / (secondNo);
+                    ans = Double.toString(answer);
+                    System.out.printf("This is your answer: %.2f\n", answer);
+                    break;
+
+                default:
+                    System.out.println("Your input is invalid");
+                    break;
+            }
 
         }
 
         System.out.println("Bye bye");
 
-        
-        //1.Accepting user input
-        //2.saving the last result
-        //3. creating switch case
-        //4. create while loop for exiting
-        
+        // 1.Accepting user input
+        // 2.saving the last result
+        // 3. creating switch case
+        // 4. create while loop for exiting
+
     }
 }
